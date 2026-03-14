1d6d33e75c411684b8699485c37e86c4d0e7dbdf
Denver Coneybeare <dconeybe@google.com>
2026-03-14 14:02:31 -0400

    kotlin multiplatform skeleton added, as generated by intellij

---
+++ composeApp/src/jvmMain/kotlin/app/rutt/rutt/main.kt
@@ -0,0 +1,13 @@
+package app.rutt.rutt
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "rutt",
+    ) {
+        App()
+    }
+}
